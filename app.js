f0d2f37c6cfbaeebe860aadd7085ad4591d73f82
Lee <lelandscanlan@gmail.com>
2016-05-23 19:26:05 -0400

    successful request to /api/v1/me

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var token;
-var qs;
 $.ready(isRedirectedURI())
 
 function isRedirectedURI() {
@@ -10,20 +9,24 @@ function isRedirectedURI() {
 		token = new URL(window.location).hash.split('&').filter(function(el) { if(el.match('access_token') !== null) return true; })[0].split('=')[1];
 
 		//endpoint that we're going to hit
-		var redditEndpoint;
+		var redditEndpoint = "https://oauth.reddit.com/api/v1/me";
+		var redditHeaders = {"Authorization": "bearer " + token};
 		
 		// call the Reddit endpoint with configured URI
-		// $.ajax({
-		// 	url: redditEndpoint,
-		// 	method: 'GET',
-		// 	dataType: 'jsonp',
-		// 	success: function(response) {
-		// 		console.log(response);
-		// 	}
-		// });
+		$.ajax({
+			url: redditEndpoint,
+			headers: redditHeaders,
+			method: 'GET',
+			dataType: 'json',
+			success: function(response) {
+				console.log(response);
+			}
+		});
 
 	}
 	else {
 		$('.article-results-view').hide();
 	}
 }
+
+// 57126528-Fl-5nx_L3X5tHnT_u5RLHqk0t9Y
