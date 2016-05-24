b11c8b4561a6f5f68e8f5702a63230db7c06004d
Lee <lelandscanlan@gmail.com>
2016-05-23 21:21:47 -0400

    add listing request, voting request, and appropriate scopes

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var token;
+var userInfo;
+var rAllData;
 $.ready(isRedirectedURI())
 
 function isRedirectedURI() {
@@ -8,25 +10,56 @@ function isRedirectedURI() {
 		//set access token
 		token = new URL(window.location).hash.split('&').filter(function(el) { if(el.match('access_token') !== null) return true; })[0].split('=')[1];
 
-		//endpoint that we're going to hit
-		var redditEndpoint = "https://oauth.reddit.com/api/v1/me";
-		var redditHeaders = {"Authorization": "bearer " + token};
-		
-		// call the Reddit endpoint with configured URI
-		$.ajax({
-			url: redditEndpoint,
-			headers: redditHeaders,
-			method: 'GET',
-			dataType: 'json',
-			success: function(response) {
-				console.log(response);
-			}
-		});
+		//get user info
+		(function() {
+			var redditEndpoint = "https://oauth.reddit.com/api/v1/me";
+			var redditHeaders = {"Authorization": "bearer " + token};
+			$.ajax({
+				url: redditEndpoint,
+				headers: redditHeaders,
+				method: 'GET',
+				dataType: 'json',
+				success: function(response) {
+					console.log(response);
+					userInfo = response;
+				}
+			});
+		})();
+		//end /api/v1/me
+
+		//get top posts on r/all
+		(function() {
+			var redditEndpoint = "https://www.reddit.com/r/all/hot.json";
+			$.ajax({
+				url: redditEndpoint,
+				method: 'GET',
+				dataType: 'json',
+				success: function(response) {
+					console.log(response);
+					rAllData = response;
+				}
+			});
+		})();
+		//end /r/all/hot
+
+		//example vote on a post
+		/*(function() {
+			var redditEndpoint = "https://oauth.reddit.com/api/vote?dir=1&id=t3_4kqr3d";
+			var redditHeaders = {"Authorization": "bearer " + token};
+			$.ajax({
+				url: redditEndpoint,
+				headers: redditHeaders,
+				method: 'POST',
+				dataType: 'json',
+				success: function(response) {
+					console.log(response);
+				}
+			});
+		})();*/
+		//end vote
 
 	}
 	else {
 		$('.article-results-view').hide();
 	}
 }
-
-// 57126528-Fl-5nx_L3X5tHnT_u5RLHqk0t9Y
